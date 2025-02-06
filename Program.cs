a4ff79bf90f1d43f3658432f4a91269c0a95bbc4
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-02-06 20:49:17 +0530

    implement method overloading concepts

--- Program.cs
+++ Program.cs
@@ -5,26 +5,30 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        Calculator calculator1 = new Calculator();
+        calculator1.add(2, 4);
+        calculator1.add(3, 5,5.5);
         //Movie Ticket Price
-        //MovieTicketPrice movieTicketPrice = new MovieTicketPrice();
-        //movieTicketPrice.CalculateTicketPrice();
+        MovieTicketPrice movieTicketPrice = new MovieTicketPrice();
+        movieTicketPrice.CalculateTicketPrice();
 
-        //LoginSystem logInSystem = new LoginSystem();
-        //logInSystem.LogIn();
+        LoginSystem logInSystem = new LoginSystem();
+        logInSystem.LogIn();
 
-        //MenuDrivenCalculator menuDrivenCalculator = new MenuDrivenCalculator();
-        //menuDrivenCalculator.Calculate();
+        MenuDrivenCalculator menuDrivenCalculator = new MenuDrivenCalculator();
+        menuDrivenCalculator.Calculate();
 
-        //CalculateAirQualityIndex calculator = new CalculateAirQualityIndex();
-        //calculator.CalculateAQI();
+        CalculateAirQualityIndex calculator = new CalculateAirQualityIndex();
+        calculator.CalculateAQI();
 
         //Shape Heirarachy
-
         Circle circle = new Circle();
         circle.GetArea();
 
         Square square = new Square();
         square.GetArea();
+
+        Shape shape = new Circle();  
 
 
 
---
+++ oopsProgramming/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicProgramming.oopsProgramming
+{
+    internal class Calculator
+    {
+        public void add(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+        public void add(double a, double b, double c){
+            double result = a + b + c;
+            Console.WriteLine(result);
+        }
+
+
+    }
+}
