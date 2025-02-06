1d501f3a12e91f33464ac51a8536fe300b9efe95
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-02-06 21:03:34 +0530

    overriding concepts

--- Program.cs
+++ Program.cs
@@ -5,9 +5,20 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        //Payment 
+        Payment payment = new CreadiCardPayment();
+        payment.ProcessPayment();
+
+        CreadiCardPayment creadiCardPayment = new CreadiCardPayment();
+        creadiCardPayment.ProcessPayment();
+
+        CashPayment cashPayment = new CashPayment();
+        cashPayment.ProcessPayment();
+
         Calculator calculator1 = new Calculator();
         calculator1.add(2, 4);
         calculator1.add(3, 5,5.5);
+
         //Movie Ticket Price
         MovieTicketPrice movieTicketPrice = new MovieTicketPrice();
         movieTicketPrice.CalculateTicketPrice();
---
+++ oopsProgramming/Payment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicProgramming.oopsProgramming
+{
+    internal class Payment
+    {
+        public void ProcessPayment()
+        {
+            Console.WriteLine("Payment is Processing ");
+        }
+
+    }
+    class CreadiCardPayment : Payment
+    {
+        public void ProcessPayment()
+        {
+            Console.WriteLine("Payment by creadit card");
+        }
+    }
+
+    class CashPayment : Payment
+    {
+        public void ProcessPayment()
+        {
+            Console.WriteLine("Payment by Cash");
+        }
+    }
+
+
+}
