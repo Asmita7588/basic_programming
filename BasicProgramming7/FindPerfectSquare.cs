24ee95309423d28de592a8e5c404329c0e5ca8b8
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-02-07 14:11:23 +0530

    Check Number weather perfect square number or not

---
+++ BasicProgramming7/FindPerfectSquare.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramming7
+{
+    internal class FindPerfectSquare
+    {
+        public void PerfectSquare(int number)
+        {
+            if (number < 0)
+                return;
+            int squareRoot = (int)Math.Sqrt(number);
+            int perfectSquare = squareRoot * squareRoot;
+
+            if (number == perfectSquare)
+                Console.WriteLine(number + " is a perfect Square Number");
+            else Console.WriteLine(number + " is not a perfect Square Number");
+
+        }
+    }
+}
--- BasicProgramming7/Program.cs
+++ BasicProgramming7/Program.cs
@@ -4,14 +4,17 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //CurrentTimeInDiffrentTimeZoneProgram currentTimeInDiffrentTimeZoneProgram = new CurrentTimeInDiffrentTimeZoneProgram();
-        //currentTimeInDiffrentTimeZoneProgram.CurrentTimeInDifferentTimeZone();
+        CurrentTimeInDiffrentTimeZoneProgram currentTimeInDiffrentTimeZoneProgram = new CurrentTimeInDiffrentTimeZoneProgram();
+        currentTimeInDiffrentTimeZoneProgram.CurrentTimeInDifferentTimeZone();
 
-        //WorkDayCalculator workDayCalculator = new WorkDayCalculator();
-        //int workedDay= workDayCalculator.CalculateWorkDay();
-        //Console.WriteLine("Total work day = " + workedDay);
+        WorkDayCalculator workDayCalculator = new WorkDayCalculator();
+        int workedDay = workDayCalculator.CalculateWorkDay();
+        Console.WriteLine("Total work day = " + workedDay);
 
         SumOddEvenOfIntegerNumber sumOddEvenOfIntegerNumber = new SumOddEvenOfIntegerNumber();
-         sumOddEvenOfIntegerNumber.SumOddEven(123456);
+        sumOddEvenOfIntegerNumber.SumOddEven(123456);
+
+        FindPerfectSquare perfectSquare = new FindPerfectSquare();
+        perfectSquare.PerfectSquare(8);
     }
 }
