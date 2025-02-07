caf49a8e6435e6fc6541f3a6e916bb51001532df
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-02-07 11:51:10 +0530

    Current time int different time zone

---
+++ BasicProgramming7/CurrentTimeInDiffrentTimeZone.Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramming7
+{
+    internal class CurrentTimeInDiffrentTimeZoneProgram
+    {
+          public void CurrentTimeInDifferentTimeZone()
+        {
+            DateTime utcTime = DateTime.UtcNow;
+            utcTime = DateTime.SpecifyKind(utcTime, DateTimeKind.Utc);
+
+            string[] timeZoneIds = {
+                "UTC",
+                "Pacific Standard Time",
+                "Eastern Standard Time", 
+                "Central European Standard Time", 
+                "India Standard Time", 
+                "China Standard Time", 
+                "Tokyo Standard Time"
+            };
+
+            foreach (string timeZoneId in timeZoneIds)
+            {
+                try
+                {
+                    TimeZoneInfo timeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+                    DateTime localTime = TimeZoneInfo.ConvertTime(utcTime, timeZone);
+                    Console.WriteLine($"Time {timeZone} : {localTime.ToString("yyyy-MM-dd HH:mm:ss")}");
+                }
+                catch (TimeZoneNotFoundException)
+                {
+                    Console.WriteLine($"Time zone '{timeZoneId}' not found.");
+                }
+                catch (InvalidTimeZoneException)
+                {
+                    Console.WriteLine($"Time zone '{timeZoneId}' is invalid.");
+                }
+            }
+        } 
+    }
+}
---
+++ BasicProgramming7/Program.cs
@@ -0,0 +1,10 @@
+﻿using BasicProgramming7;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CurrentTimeInDiffrentTimeZoneProgram currentTimeInDiffrentTimeZoneProgram = new CurrentTimeInDiffrentTimeZoneProgram();
+        currentTimeInDiffrentTimeZoneProgram.CurrentTimeInDifferentTimeZone();
+    }
+}
