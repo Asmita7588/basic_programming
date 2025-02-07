8b88b16da69e3bb71c4a425b6c55876c30549527
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-02-07 14:39:19 +0530

    check unique character within string

---
+++ BasicProgramming7/CheckUnqueCharWithinString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramming7
+{
+    internal class CheckUnqueCharWithinString
+    {
+        public bool CheckUnqueString(string input) {
+
+            input = input.ToLower();
+            for (int i = 0; i < input.Length; i++) {
+                char ch = input[i];
+                if (input.IndexOf(ch) != input.LastIndexOf(ch) )
+                {
+                   return false;
+                }
+                
+            }
+          return true;
+        }
+        
+    } 
+}
+
--- BasicProgramming7/Program.cs
+++ BasicProgramming7/Program.cs
@@ -4,17 +4,28 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        CurrentTimeInDiffrentTimeZoneProgram currentTimeInDiffrentTimeZoneProgram = new CurrentTimeInDiffrentTimeZoneProgram();
-        currentTimeInDiffrentTimeZoneProgram.CurrentTimeInDifferentTimeZone();
+        //CurrentTimeInDiffrentTimeZoneProgram currentTimeInDiffrentTimeZoneProgram = new CurrentTimeInDiffrentTimeZoneProgram();
+        //currentTimeInDiffrentTimeZoneProgram.CurrentTimeInDifferentTimeZone();
 
-        WorkDayCalculator workDayCalculator = new WorkDayCalculator();
-        int workedDay = workDayCalculator.CalculateWorkDay();
-        Console.WriteLine("Total work day = " + workedDay);
+        //WorkDayCalculator workDayCalculator = new WorkDayCalculator();
+        //int workedDay = workDayCalculator.CalculateWorkDay();
+        //Console.WriteLine("Total work day = " + workedDay);
 
-        SumOddEvenOfIntegerNumber sumOddEvenOfIntegerNumber = new SumOddEvenOfIntegerNumber();
-        sumOddEvenOfIntegerNumber.SumOddEven(123456);
+        //SumOddEvenOfIntegerNumber sumOddEvenOfIntegerNumber = new SumOddEvenOfIntegerNumber();
+        //sumOddEvenOfIntegerNumber.SumOddEven(123456);
 
-        FindPerfectSquare perfectSquare = new FindPerfectSquare();
-        perfectSquare.PerfectSquare(8);
+        //FindPerfectSquare perfectSquare = new FindPerfectSquare();
+        //perfectSquare.PerfectSquare(8);
+
+        CheckUnqueCharWithinString checkUnqueCharWithinString = new CheckUnqueCharWithinString();
+        bool isunique = checkUnqueCharWithinString.CheckUnqueString("Asmita");
+
+        if(isunique) 
+            Console.WriteLine("given String  has Unique Characters");
+            else 
+            Console.WriteLine("given String has not Unique Characters");
+           
+
+
     }
 }
