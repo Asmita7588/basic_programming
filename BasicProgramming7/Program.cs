b985d1e812e04c6169d8775c1b71ab77fadaa974
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-02-07 13:27:39 +0530

    Calculate total worked day except saturday sunday

--- BasicProgramming7/Program.cs
+++ BasicProgramming7/Program.cs
@@ -4,7 +4,11 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        CurrentTimeInDiffrentTimeZoneProgram currentTimeInDiffrentTimeZoneProgram = new CurrentTimeInDiffrentTimeZoneProgram();
-        currentTimeInDiffrentTimeZoneProgram.CurrentTimeInDifferentTimeZone();
+        //CurrentTimeInDiffrentTimeZoneProgram currentTimeInDiffrentTimeZoneProgram = new CurrentTimeInDiffrentTimeZoneProgram();
+        //currentTimeInDiffrentTimeZoneProgram.CurrentTimeInDifferentTimeZone();
+
+        WorkDayCalculator workDayCalculator = new WorkDayCalculator();
+        int workedDay= workDayCalculator.CalculateWorkDay();
+        Console.WriteLine("Total work day " + workedDay);
     }
 }
---
+++ BasicProgramming7/WorkDayCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramming7
+{
+    internal class WorkDayCalculator
+    {
+        public int CalculateWorkDay() {
+
+            Console.Write("Enter Start Date in this Format (YYYY - MM - DD): ");
+            DateTime startDate = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Enter End Date in this Format (YYYY - MM - DD): ");
+            DateTime endDate = DateTime.Parse(Console.ReadLine());
+            int workDay = 0;
+            if(startDate > endDate)
+            {
+                Console.WriteLine("Start date should be greater than End Date");
+
+            }
+            while(startDate <= endDate)
+            {
+                if(startDate.DayOfWeek != DayOfWeek.Saturday && startDate.DayOfWeek != DayOfWeek.Saturday)
+                {
+                    workDay++;
+                }
+                startDate = startDate.AddDays(1);
+
+            }
+            return workDay;
+        }
+    }
+}
